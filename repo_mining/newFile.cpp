34a8eb6edda7f81056e04c24613165023aded5c0
Heath Mensch <mensch1@unlv.nevada.edu>
2026-01-26 15:12:55 -0800

    Adding newFile.cpp that has a function to add numbers

---
+++ repo_mining/newFile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int add (int x, int y) {
+    int final = x + y;
+    return final;
+}
+
+int main() {
+    cout << add(4,6) << endl;
+    return 0;
+}
